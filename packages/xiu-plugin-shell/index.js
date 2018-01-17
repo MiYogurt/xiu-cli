19a382ab99011d8a5a5786a51c4802675c9e7e2b
yugo <belovedyogurt@gmail.com>
2018-01-17 13:00:40 +0800

    publish 0.0.4

---
+++ packages/xiu-plugin-shell/index.js
@@ -0,0 +1,10 @@
+
+function shell(task, shell){
+    return shell(task.shell).then(() => false)
+}
+
+module.exports = module.exports.default = exports = shell
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
