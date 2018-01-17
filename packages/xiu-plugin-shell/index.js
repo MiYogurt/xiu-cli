852abe8039caee7769530f25cf2bc3f9475bb705
yugo <belovedyogurt@gmail.com>
2018-01-17 19:30:28 +0800

    ⛄️ add pritter and eslint

--- packages/xiu-plugin-shell/index.js
+++ packages/xiu-plugin-shell/index.js
@@ -1,10 +1,9 @@
-
-function shell(task, shell){
-    return shell(task.shell).then(() => false)
+function shell(task, shell) {
+	return shell(task.shell).then(() => false)
 }
 
 module.exports = module.exports.default = exports = shell
 
-Object.defineProperty(exports, "__esModule", {
-    value: true
-});
+Object.defineProperty(exports, '__esModule', {
+	value: true
+})
--- src/task.js
+++ src/task.js
@@ -7,70 +7,74 @@ import chalk from 'chalk'
 import runscript from 'runscript'
 
 function read_file(path) {
-    return fs.readFileSync(join(path, 'xiu.toml')).toString()
+	return fs.readFileSync(join(path, 'xiu.toml')).toString()
 }
 
+const _skipName = ['skip', 'name']
 
-const _skipName = ['skip', 'name' ]
-
-function get_plugin(name){
-    try{
-        const plugin = require(`xiu-plugin-${name}`)
-        return plugin
-    }catch(e){
-        console.log(chalk.magenta(`>>>>>> ❌ [ xiu-plugin-${name} ] Not Found <<<<<<`))
-        return null;
-    }
+function get_plugin(name) {
+	try {
+		const plugin = require(`xiu-plugin-${name}`)
+		return plugin
+	} catch (e) {
+		console.log(
+			chalk.magenta(`>>>>>> ❌ [ xiu-plugin-${name} ] Not Found <<<<<<`)
+		)
+		return null
+	}
 }
 
 export async function queue({ env, pipes, ignore = [] }, cwd) {
-    const run = (text) => runscript(text, {
-        cwd,
-        env
-    });
-    try {
-        for(let pipe of pipes) {
+	const run = text =>
+		runscript(text, {
+			cwd,
+			env
+		})
+	for (let pipe of pipes) {
+		// 设置了跳过则直接跳过
+		if (pipe.skip) {
+			console.log(
+				chalk.magenta('>>>>>> 🚧 [ ') +
+					chalk.underline(pipe.name) +
+					chalk.magenta(' ] Skip The Task <<<<<<')
+			)
+			continue
+		}
 
-            // 设置了跳过则直接跳过
-            if (pipe.skip) {
-                console.log(chalk.magenta(`>>>>>> 🚧 [ `) + chalk.underline(pipe.name) + chalk.magenta(` ] Skip The Task <<<<<<`))
-                continue
-            }
+		// 过滤掉不用的属性
+		const taskNames = Object.keys(pipe).filter(
+			key => _skipName.concat(ignore).indexOf(key) < 0
+		)
 
-            // 过滤掉不用的属性
-            const taskNames = Object.keys(pipe).filter((key) => _skipName.concat(ignore).indexOf(key) < 0);
+		// 将属性名映射成插件明
+		for (let name of taskNames) {
+			let plugin = get_plugin(name)
 
-            // 将属性名映射成插件明
-            for(let name of taskNames){
+			if (!plugin) {
+				//
+				process.exit(-1)
+			}
 
-                let plugin = get_plugin(name);
+			if (plugin.default) {
+				plugin = plugin.default
+			}
 
-                if (!plugin) {
-                    process.exit(-1)
-                }
-
-                if (plugin.default) {
-                    plugin = plugin.default
-                }
-
-                console.log(chalk.cyanBright(`>>>>>> 🚀 [ `) + chalk.underline(pipe.name) + chalk.cyanBright(` ] Running Task <<<<<<`))
-                try{
-                    const skip = await plugin(pipe, run)
-                    if (skip) {
-                        continue
-                    }
-                }catch(e){
-                    console.log(e)
-                }
-            }
-        }
-        console.log("\n");
-    }catch (e) {
-        console.log(chalk.red(e))
-    }
+			console.log(
+				chalk.cyanBright('>>>>>> 🚀 [ ') +
+					chalk.underline(pipe.name) +
+					chalk.cyanBright(' ] Running Task <<<<<<')
+			)
+			await plugin(pipe, run)
+		}
+	}
+	console.log('\n')
 }
 
-export function start(path){
-    const config = read_file(path)
-    queue(toml.parse(config), path)
+export function start(path) {
+	try {
+		const config = read_file(path)
+		queue(toml.parse(config), path)
+	} catch (e) {
+		console.error(e)
+	}
 }
